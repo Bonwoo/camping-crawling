4bbad5805c29b5efdd10da102977f03a9069f0eb
Bonwoo <kbw0017@naver.com>
2020-07-16 11:20:47 +0900

    test

--- camping.py
+++ camping.py
@@ -6,19 +6,25 @@ import lib_main
 #날짜 입력 전 기본 URL
 DEFAULT_URL = 'https://camping.gtdc.or.kr/DZ_reservation/reserCamping.php?xch=reservation&xid=camping_reservation&searchDate='             
             
-INTERVAL_SECOND = 10 # 조회간격(초)
+INTERVAL_SECOND = 5 # 조회간격(초)
 
-def getStock(url):
+def getInfo(url, date, area):
     #bot 감지 차단을 막기위한 헤더조작
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
     data = requests.get(url, headers = headers)
     bsObj = BeautifulSoup(data.content, "html.parser")
     #print(bsObj) #접속안될때 풀어 보는 용도 
 
-    result = bsObj.find('input', class_=r'prod-quantity__input') # 재고있는 상품은 1로 뜸
-    remain = result.get('value') #string
+    
+    value = area+":"+date[:4]+"-"+date[4:6]+"-"+date[6:8] # 버튼에 있는 value 값
+    #print('value = {}'.format(value)) # TEST 출력
 
-    return remain
+    
+    result = bsObj.find('button', {"value":value}) # 재고있는 상품은 1로 뜸
+    
+    print(result)    
+
+    return result
 
 # 현재시간 리턴
 def getTime():
@@ -27,15 +33,8 @@ def getTime():
 # 텔레그램 메시지 전송
 def sendMsg(msg):
     print(msg)
-    lib_main.sendTelegram(msg) # 텔레그램 전송
+    #lib_main.sendTelegram(msg) # 텔레그램 전송
 
-# 마스크 리스트를 출력하여 어떤 마스크를 조회할지 알아본다
-def getMask():
-    index = int(lib_main.choiceLIST(MASK_LIST)) - 1 # 입력받은 마스크 항목을 인덱스 번호로 저장        
-    mask = list(MASK_LIST.keys())[index]
-    print('{}를 선택하셨습니다.'.format(mask))
-    
-    return mask
 
 # URL
 def getUrl(date):
@@ -64,30 +63,32 @@ def startCrawler():
     print('작업일자 : {}'.format(date))
     print('작업구역 : {}'.format(area))
     print('URL : {}'.format(url))
-    '''       
+           
     if url is None :
         msg = '크롤러[{}]를 시작할 수 없습니다.({})'.format(url, getTime())
         sendMsg(msg)
         return
     
-    msg = '쿠팡 크롤러[{}]를 시작합니다.({})'.format(mask, getTime())
+    msg = '크롤러를 시작합니다.(작업일자{}작업구역{})'.format(date, area)
     sendMsg(msg)
     sendMsg(url)
     
-    # 크롤링 작업 진행(10초마다[INTERVAL_SECOND])
+    # 크롤링 작업 진행(5초마다[INTERVAL_SECOND])
     while 1:
-        stock = getStock(url)        
-        msg = '주문가능 수량({}) : {}'.format(getTime(), stock)        
-        print(msg)
+        result = getInfo(url, date, area)        
+        #msg = '주문가능 수량({}) : {}'.format(getTime(), result)        
+        #print(msg)
 
         # 수량 확인
-        if stock != '0':
-            msg = '수량 확인({}) : {}'.format(getTime(), stock)            
+        if result is None:
+            print('수량 없음({})'.format(getTime()))
+        else:
+            msg = '수량 확인({}) : {}'.format(getTime(), result)            
             sendMsg(msg)
             sendMsg(url)           
             
         time.sleep(INTERVAL_SECOND)
-    '''       
+
 
 
 # MAIN 으로 동작시키기 
--- lib_main.py
+++ lib_main.py
@@ -2,7 +2,6 @@ import sys
 import lib_telegram #telegram.py
 from datetime import datetime
 
-PG_LIST = { '쿠팡' : 1, '웰킵스' : 2}
 
 # 현재시간 리턴
 def getTime():
@@ -13,26 +12,3 @@ def sendTelegram(msg):
     lib_telegram.sendTelegram(msg)
     
 
-# 딕셔너리 값을 화면에 표시하여 값을 읽음
-def choiceLIST(list):
-    while True :
-        i = 0
-        # for 상품출력
-        for key in list.keys():
-            i = i + 1
-            print('{}. {}'.format(str(i), key))
-
-        choice = input('숫자를 입력하세요[Q:종료] : ')
-
-        # Q를 입력하면 종료
-        if str(choice).upper == 'Q' :
-            print('프로그램을 종료합니다.')
-            sys.exit()            
-
-        if choice.isdecimal == False: # 숫자만 입력가능
-            continue
-        elif int(choice) > i : #LIST보다 큰 값을 입력한 경우            
-            continue
-        else :
-            print(choice)
-            return choice
