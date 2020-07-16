55cdc2e88d2a6d6484b28848fb000ad1ccf45e08
Bonwoo <kbw0017@naver.com>
2020-07-16 12:26:39 +0900

    ㅊ

--- lib_telegram.py
+++ lib_telegram.py
@@ -4,14 +4,15 @@ def sendTelegram(msg):
     my_token = r'1163113362:AAFtIm0BSzQEFVFR9ArwkBBpP2vALYn00NU'
     my_chatid = '1260392260'
     bot = telegram.Bot(token = my_token)
-    """
-    TEST 출력
+    
+    #TEST 출력
+    
     updates = bot.getUpdates() 
     print(bot)   
     
     for u in updates: 
         print(u.message)
-    """
+    
     bot.sendMessage(chat_id = my_chatid, text = msg)
 
         
