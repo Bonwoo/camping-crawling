16db2ae696395cd0a8f7617cd74ef5d5c75cd1c2
Bonwoo <kbw0017@naver.com>
2020-07-16 11:31:19 +0900

    telegram 변경

--- lib_telegram.py
+++ lib_telegram.py
@@ -2,6 +2,7 @@ import telegram
 
 def sendTelegram(msg):
     my_token = r'1163113362:AAFtIm0BSzQEFVFR9ArwkBBpP2vALYn00NU'
+    my_chatid = '1260392260'
     bot = telegram.Bot(token = my_token)
     """
     TEST 출력
@@ -11,6 +12,6 @@ def sendTelegram(msg):
     for u in updates: 
         print(u.message)
     """
-    bot.sendMessage(chat_id = '1260392260', text = msg)
+    bot.sendMessage(chat_id = my_chatid, text = msg)
 
         
